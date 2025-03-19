5acd88d94e2e887ff730c76befa560cab2ebb145
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-19 23:48:19 +0530

    add navigation links to Navbar component for Sell, Buy, and Rent options

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,15 @@ const Navbar = () => {
         alt="Logo"
         className="w-[80px] h-[39px]"
       />
+
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-4">
+          <p className="text-[#5f5f5f]">Sell</p>
+          <p className="text-[#5f5f5f]">Buy</p>
+          <p className="text-[#5f5f5f]">Rent</p>
+        </div>
+        <div className="flex items-center gap-4">
+            
     </div>
   );
 };
