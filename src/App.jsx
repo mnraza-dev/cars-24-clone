331ab8aefad3fd21420e5e3caeefb925d2038cc4
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-20 00:07:25 +0530

    add Footer component with logo and tagline to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import Footer from "./components/Footer";
 
 function App() {
   return <div>
     <Navbar />
     <HeroSection/>
+    <Footer/>
   </div>;
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bg-[#F5F5F5] mx-auto w-full">
+      <div className="flex gap-2 items-center">
+        <img
+          src="https://media.cars24.com/cars24/seo/static/1_20230830_1693395013.png"
+          alt="Logo"
+          className="w-[78px] h-[28px]"
+        />
+        <p className="text-2xl"> Better drives, better lives</p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
