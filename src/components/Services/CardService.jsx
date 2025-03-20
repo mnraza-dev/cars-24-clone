b5662f48fbdf05c860d11f8475db440aa8de80c8
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-20 23:41:17 +0530

    update CardService component to include an image for better visual representation

--- src/components/Services/CardService.jsx
+++ src/components/Services/CardService.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
 const CardService = () => {
   return (
-    <div>CardService</div>
+    <div>
+        <img src="	https://cdn.24c.in/prod/super-app/root/2024/07/24/…8c45b-7ee1-4f80-8fde-4dc61ef49c70-Drive-home.webp" alt="Buy_Car_Services" />
+    </div>
   )
 }
 
