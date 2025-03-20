087b6c4a2e1bd13bf7a0539f79e04a8e57e3c674
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-20 23:59:54 +0530

    refactor CardServiceItem layout for improved spacing and styling consistency

--- src/components/Services/CardServiceItem.jsx
+++ src/components/Services/CardServiceItem.jsx
@@ -3,19 +3,21 @@ const CardServiceItem = () => {
   return (
     <div className="m-4  rounded-2xl  drop-shadow-md max-w-[300px] min-[300px]    border-2  border-gray-200 shadow-2xl">
       <div className="m-0 ">
-      <img
-        src="https://cdn.24c.in/prod/super-app/root/2024/07/24/8898c45b-7ee1-4f80-8fde-4dc61ef49c70-Drive-home.webp"
-        alt="BUY"
-        className="w-full rounded-b-4xl"
-      />
-      <div className="services"></div>
+        <img
+          src="https://cdn.24c.in/prod/super-app/root/2024/07/24/8898c45b-7ee1-4f80-8fde-4dc61ef49c70-Drive-home.webp"
+          alt="BUY"
+          className="w-full rounded-xl rounded-b-4xl"
+        />
+        <div className="services"></div>
       </div>
-      <div className="text-2xl font-semibold">Buy _________</div>
-      <div>
-        <h2>Drive home your dream car</h2>
-      </div>
-      <div>
-        <button className="bg-[#F5F5F5] ">View all cars</button>
+      <div className="p-4">
+        <div className="text-2xl font-semibold">Buy _________</div>
+        <div>
+          <h2>Drive home your dream car</h2>
+        </div>
+        <div>
+          <button className="bg-[#F5F5F5] ">View all cars</button>
+        </div>
       </div>
     </div>
   );
