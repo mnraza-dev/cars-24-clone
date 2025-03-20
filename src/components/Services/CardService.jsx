a03eed26c7a933bd6cd6dfee5412f35b85424885
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-20 23:42:29 +0530

    update CardService component to enhance layout and include service title

--- src/components/Services/CardService.jsx
+++ src/components/Services/CardService.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react";
 const CardService = () => {
   return (
-    <div>
-        <img src="	https://cdn.24c.in/prod/super-app/root/2024/07/24/…8c45b-7ee1-4f80-8fde-4dc61ef49c70-Drive-home.webp" alt="Buy_Car_Services" />
-    </div>
-  )
-}
+    <div className=" ">
+      <img
+        src="	https://cdn.24c.in/prod/super-app/root/2024/07/24/…8c45b-7ee1-4f80-8fde-4dc61ef49c70-Drive-home.webp"
+        alt="Buy_Car_Services"
+        className=""
+      />
 
-export default CardService
+      <div className="text-2xl font-semibold">Buy _________</div>
+      
+    </div>
+  );
+};
+
+export default CardService;
