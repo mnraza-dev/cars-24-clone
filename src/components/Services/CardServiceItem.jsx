7872543908e19f3a4c88b965881c5d219d9c4dcb
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-20 23:56:37 +0530

    refactor CardServiceItem layout for improved structure and styling

--- src/components/Services/CardServiceItem.jsx
+++ src/components/Services/CardServiceItem.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 const CardServiceItem = () => {
   return (
-    <div className=" p-2 rounded-2xl  drop-shadow-md max-w-[300px] min-[300px]  ">
+    <div className="  rounded-2xl  drop-shadow-md max-w-[300px] min-[300px]  ">
+      <div className="m-0 ">
       <img
         src="https://cdn.24c.in/prod/super-app/root/2024/07/24/8898c45b-7ee1-4f80-8fde-4dc61ef49c70-Drive-home.webp"
         alt="BUY"
-        className="w-full rounded-b-2xl"
+        className="w-full rounded-b-4xl"
       />
       <div className="services"></div>
+      </div>
       <div className="text-2xl font-semibold">Buy _________</div>
       <div>
         <h2>Drive home your dream car</h2>
