8db0c6f7afc4930db00fdfbc4dceac0f01250e0d
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-20 23:47:23 +0530

    update App component to include CardServices and enhance layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import Footer from "./components/Footer";
+import CardServices from "./components/Services/CardServices";
 
 function App() {
-  return <div>
-    <Navbar />
-    <HeroSection/>
-    <Footer/>
-  </div>;
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+      <CardServices />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Services/CardServiceItem.jsx
+++ src/components/Services/CardServiceItem.jsx
@@ -2,12 +2,7 @@ import React from "react";
 const CardServiceItem = () => {
   return (
     <div className=" ">
-      <img
-        src="	https://cdn.24c.in/prod/super-app/root/2024/07/24/…8c45b-7ee1-4f80-8fde-4dc61ef49c70-Drive-home.webp"
-        alt="Buy_Car_Services"
-        className=""
-      />
-
+      <img src="https://cdn.24c.in/prod/super-app/root/2024/07/24/8898c45b-7ee1-4f80-8fde-4dc61ef49c70-Drive-home.webp" alt="BUY"  className=""/>
       <div className="text-2xl font-semibold">Buy _________</div>
       <div>
         <h2>Drive home your dream car</h2>
